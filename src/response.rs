437982bc1fb858655efcc3e4119e7249d7fc56bc
takenoko-gohan <bamboo.shoot.rice@gmail.com>
2023-07-31 05:58:53 +0900

    Added mime and mime_guess dependencies and refactored get_s3_object
    The 'mime' and 'mime_guess' crates were added to improve the handling of various MIME types. 'mime_guess' is now utilized to automatically estimate the correct MIME type based on file extensions when we return content from the S3 bucket, thus reducing the manual mime type listings greatly. This makes our response handling more accurate and support more extensive range of file types without specifying each. The 'get_s3_object' function is also refactored to handle S3 errors in a separate function, decluttering the code and improving readability.

--- src/response.rs
+++ src/response.rs
@@ -1,9 +1,12 @@
+use aws_sdk_s3::error::SdkError;
+use aws_sdk_s3::operation::get_object::GetObjectError;
 use aws_sdk_s3::Client;
 use bytes::Bytes;
 use http_body_util::combinators::BoxBody;
 use http_body_util::{BodyExt, Full};
-use hyper::{Response, StatusCode};
+use hyper::StatusCode;
 
+type Response = hyper::Response<BoxBody<Bytes, hyper::Error>>;
 type Error = Box<dyn std::error::Error + Send + Sync>;
 
 fn full<T: Into<Bytes>>(chunk: T) -> BoxBody<Bytes, hyper::Error> {
@@ -12,9 +15,7 @@ fn full<T: Into<Bytes>>(chunk: T) -> BoxBody<Bytes, hyper::Error> {
         .boxed()
 }
 
-pub fn easy_response(
-    status_code: StatusCode,
-) -> Result<Response<BoxBody<Bytes, hyper::Error>>, Error> {
+pub fn easy_response(status_code: StatusCode) -> Result<Response, Error> {
     let body = match status_code {
         StatusCode::OK => full("OK"),
         StatusCode::BAD_REQUEST => full("Bad Request"),
@@ -23,7 +24,7 @@ pub fn easy_response(
         _ => full("Unknown"),
     };
 
-    Ok(Response::builder()
+    Ok(hyper::Response::builder()
         .header("Content-Type", "text/plain")
         .status(status_code)
         .body(body)?)
@@ -34,23 +35,10 @@ pub async fn s3_object_response(
     bucket: &str,
     key: &str,
     no_such_key_redirect_path: Option<String>,
-) -> Result<Response<BoxBody<Bytes, hyper::Error>>, Error> {
+) -> Result<Response, Error> {
     let s3_obj = match s3_client.get_object().bucket(bucket).key(key).send().await {
         Ok(obj) => obj,
-        Err(e) => {
-            tracing::error!("Failed to get object: {:?}", e);
-            return if e.into_service_error().is_no_such_key() {
-                match no_such_key_redirect_path {
-                    Some(redirect_path) => Ok(Response::builder()
-                        .status(StatusCode::FOUND)
-                        .header("Location", redirect_path)
-                        .body(full("Found"))?),
-                    None => easy_response(StatusCode::NOT_FOUND),
-                }
-            } else {
-                easy_response(StatusCode::INTERNAL_SERVER_ERROR)
-            };
-        }
+        Err(e) => return get_s3_object_error(e, no_such_key_redirect_path),
     };
 
     let b = match s3_obj.body.collect().await {
@@ -61,26 +49,29 @@ pub async fn s3_object_response(
         }
     };
 
-    let res = Response::builder().status(StatusCode::OK);
-    let res = match key.split('.').last() {
-        Some("txt") => res.header("Content-Type", "text/plain"),
-        Some("html") => res.header("Content-Type", "text/html"),
-        Some("css") => res.header("Content-Type", "text/css"),
-        Some("js") => res.header("Content-Type", "text/javascript"),
-        Some("xml") => res.header("Content-Type", "text/xml"),
-        Some("csv") => res.header("Content-Type", "text/csv"),
-        Some("png") => res.header("Content-Type", "image/png"),
-        Some("jpg") => res.header("Content-Type", "image/jpeg"),
-        Some("jpeg") => res.header("Content-Type", "image/jpeg"),
-        Some("gif") => res.header("Content-Type", "image/gif"),
-        Some("svg") => res.header("Content-Type", "image/svg+xml"),
-        Some("webp") => res.header("Content-Type", "image/webp"),
-        Some("ico") => res.header("Content-Type", "image/vnd.microsoft.icon"),
-        Some("json") => res.header("Content-Type", "application/json"),
-        Some("pdf") => res.header("Content-Type", "application/pdf"),
-        _ => res.header("Content-Type", "text/plain"),
-    };
-    let res = res.body(full(b))?;
+    let content_type = mime_guess::from_path(key)
+        .first_or(mime::TEXT_PLAIN)
+        .to_string();
+    Ok(hyper::Response::builder()
+        .status(StatusCode::OK)
+        .header("Content-Type", content_type)
+        .body(full(b))?)
+}
 
-    Ok(res)
+fn get_s3_object_error(
+    error: SdkError<GetObjectError>,
+    no_such_key_redirect_path: Option<String>,
+) -> Result<Response, Error> {
+    tracing::warn!("Failed to get object: {:?}", error);
+    if error.into_service_error().is_no_such_key() {
+        match no_such_key_redirect_path {
+            Some(redirect_path) => Ok(hyper::Response::builder()
+                .status(StatusCode::FOUND)
+                .header("Location", redirect_path)
+                .body(full("Found"))?),
+            None => easy_response(StatusCode::NOT_FOUND),
+        }
+    } else {
+        easy_response(StatusCode::INTERNAL_SERVER_ERROR)
+    }
 }
