30882e0244709fe03d7026dbc06fec30fa0fef1e
takenoko-gohan <bamboo.shoot.rice@gmail.com>
2023-07-30 13:26:32 +0900

    Refactor S3 object retrieval and response preparation
    This change extracts and abstracts the logic of S3 object retrieval and HTTP response preparation into a dedicated function called `s3_object_response` in the 'response' module. This incorporates error handling, S3 object retrieval, content-type determination based on file extension and response building.
    
    The extracted function is then used in the 'handle' module, replacing a large amount of redundant and less readable code. This change enhances the maintainability and readability of the code and improves the application's robustness in handling S3 files and HTTP responses.

--- src/handle.rs
+++ src/handle.rs
@@ -22,52 +22,7 @@ pub async fn handler(
     let key = key.trim_start_matches('/');
     tracing::info!("host: {}, key: {}", host, key);
 
-    let s3_obj = match s3_client
-        .get_object()
-        .bucket(host.to_string())
-        .key(key)
-        .send()
-        .await
-    {
-        Ok(obj) => obj,
-        Err(e) => {
-            tracing::error!("Failed to get object: {:?}", e);
-            return if e.into_service_error().is_no_such_key() {
-                response::easy_response(StatusCode::NOT_FOUND)
-            } else {
-                response::easy_response(StatusCode::INTERNAL_SERVER_ERROR)
-            };
-        }
-    };
-
-    let b = match s3_obj.body.collect().await {
-        Ok(b) => b.into_bytes(),
-        Err(e) => {
-            tracing::error!("Failed to collect body: {:?}", e);
-            return response::easy_response(StatusCode::INTERNAL_SERVER_ERROR);
-        }
-    };
-
-    let res = Response::builder().status(StatusCode::OK);
-    let res = match key.split('.').last() {
-        Some("txt") => res.header("Content-Type", "text/plain"),
-        Some("html") => res.header("Content-Type", "text/html"),
-        Some("css") => res.header("Content-Type", "text/css"),
-        Some("js") => res.header("Content-Type", "text/javascript"),
-        Some("xml") => res.header("Content-Type", "text/xml"),
-        Some("png") => res.header("Content-Type", "image/png"),
-        Some("jpg") => res.header("Content-Type", "image/jpeg"),
-        Some("jpeg") => res.header("Content-Type", "image/jpeg"),
-        Some("gif") => res.header("Content-Type", "image/gif"),
-        Some("svg") => res.header("Content-Type", "image/svg+xml"),
-        Some("webp") => res.header("Content-Type", "image/webp"),
-        Some("ico") => res.header("Content-Type", "image/x-icon"),
-        Some("json") => res.header("Content-Type", "application/json"),
-        _ => res.header("Content-Type", "text/plain"),
-    };
-    let res = res.body(response::full(b))?;
-
-    Ok(res)
+    response::s3_object_response(s3_client, &host, key).await
 }
 
 fn get_host(headers: &HeaderMap) -> Result<String, Error> {
--- src/response.rs
+++ src/response.rs
@@ -1,3 +1,4 @@
+use aws_sdk_s3::Client;
 use bytes::Bytes;
 use http_body_util::combinators::BoxBody;
 use http_body_util::{BodyExt, Full};
@@ -5,7 +6,7 @@ use hyper::{Response, StatusCode};
 
 type Error = Box<dyn std::error::Error + Send + Sync>;
 
-pub fn full<T: Into<Bytes>>(chunk: T) -> BoxBody<Bytes, hyper::Error> {
+fn full<T: Into<Bytes>>(chunk: T) -> BoxBody<Bytes, hyper::Error> {
     Full::new(chunk.into())
         .map_err(|never| match never {})
         .boxed()
@@ -26,4 +27,51 @@ pub fn easy_response(
         .header("Content-Type", "text/plain")
         .status(status_code)
         .body(body)?)
+}
+
+pub async fn s3_object_response(
+    s3_client: Client,
+    bucket: &str,
+    key: &str,
+) -> Result<Response<BoxBody<Bytes, hyper::Error>>, Error> {
+    let s3_obj = match s3_client.get_object().bucket(bucket).key(key).send().await {
+        Ok(obj) => obj,
+        Err(e) => {
+            tracing::error!("Failed to get object: {:?}", e);
+            return if e.into_service_error().is_no_such_key() {
+                easy_response(StatusCode::NOT_FOUND)
+            } else {
+                easy_response(StatusCode::INTERNAL_SERVER_ERROR)
+            };
+        }
+    };
+
+    let b = match s3_obj.body.collect().await {
+        Ok(b) => b.into_bytes(),
+        Err(e) => {
+            tracing::error!("Failed to collect body: {:?}", e);
+            return easy_response(StatusCode::INTERNAL_SERVER_ERROR);
+        }
+    };
+
+    let res = Response::builder().status(StatusCode::OK);
+    let res = match key.split('.').last() {
+        Some("txt") => res.header("Content-Type", "text/plain"),
+        Some("html") => res.header("Content-Type", "text/html"),
+        Some("css") => res.header("Content-Type", "text/css"),
+        Some("js") => res.header("Content-Type", "text/javascript"),
+        Some("xml") => res.header("Content-Type", "text/xml"),
+        Some("png") => res.header("Content-Type", "image/png"),
+        Some("jpg") => res.header("Content-Type", "image/jpeg"),
+        Some("jpeg") => res.header("Content-Type", "image/jpeg"),
+        Some("gif") => res.header("Content-Type", "image/gif"),
+        Some("svg") => res.header("Content-Type", "image/svg+xml"),
+        Some("webp") => res.header("Content-Type", "image/webp"),
+        Some("ico") => res.header("Content-Type", "image/x-icon"),
+        Some("json") => res.header("Content-Type", "application/json"),
+        _ => res.header("Content-Type", "text/plain"),
+    };
+    let res = res.body(full(b))?;
+
+    Ok(res)
 }
